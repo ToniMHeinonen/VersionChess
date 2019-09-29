889c69bd4053313fb22e571426d21064958a164b
Toni <toni.heinonen@hotmail.fi>
2019-09-29 15:46:44 +0300

    Add positions 2D array to Board
    - Positions array stores all the chess pieces locations
    - Initialized size as 8x8 since that is the size of the chess board

--- Board.java
+++ Board.java
@@ -1,11 +1,13 @@
 public class Board {
 
+    private ChessPiece[][] positions = new ChessPiece[8][8];
+
     public Board() {
         initializeChess();
     }
 
     private void initializeChess() {
-
+        
     }
 
     public void play() {
