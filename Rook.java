73c140ad936124247e3c0f917f90a36041b88715
Ossi <ollimatti.aikio@cs.tamk.fi>
2019-09-30 17:04:51 +0300

    Add symbols to Rook

--- Rook.java
+++ Rook.java
@@ -2,5 +2,10 @@ public class Rook extends ChessPiece {
 
     public Rook(int x, int y, int player) {
         super(x, y, player);
+
+        if (player == 1)
+            setSymbol("R");
+        else
+            setSymbol("r");
     }
 }
