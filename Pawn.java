65303a27232f96894e3cebac25439864701ee691
Ossi <ollimatti.aikio@cs.tamk.fi>
2019-09-30 17:04:03 +0300

    Add symbols to Pawn

--- Pawn.java
+++ Pawn.java
@@ -2,5 +2,10 @@ public class Pawn extends ChessPiece {
     
     public Pawn(int x, int y, int player) {
         super(x, y, player);
+
+        if (player == 1)
+            setSymbol("P");
+        else
+            setSymbol("p");
     }
 }
