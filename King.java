a933ad70aa8aedfba87a576675054f5c2334ad31
Ossi <ollimatti.aikio@cs.tamk.fi>
2019-09-30 17:06:14 +0300

    Add symbol to King

--- King.java
+++ King.java
@@ -2,5 +2,10 @@ public class King extends ChessPiece {
 
     public King(int x, int y, int player) {
         super(x, y, player);
+
+        if (player == 1)
+            setSymbol("K");
+        else
+            setSymbol("k");
     }
 }
