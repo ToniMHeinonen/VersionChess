e9c12db46936722065b958fdff8986ca28470627
Ossi <ollimatti.aikio@cs.tamk.fi>
2019-09-30 17:07:18 +0300

    Add symbol to Knight

--- Knight.java
+++ Knight.java
@@ -2,5 +2,10 @@ public class Knight extends ChessPiece {
     
     public Knight(int x, int y, int player) {
         super(x, y, player);
+
+        if (player == 1)
+            setSymbol("H");
+        else
+            setSymbol("h");
     }
 }
