b408323054ea624c8293e2a9815558b65590ea19
Toni <toni.heinonen@hotmail.fi>
2019-10-01 12:17:04 +0300

    Add symbol init to King in constructor

--- King.java
+++ King.java
@@ -2,5 +2,10 @@ public class King extends ChessPiece {
 
     public King(int row, int col, int player) {
         super(row, col, player);
+
+        if (player == 1)
+            setSymbol("K");
+        else
+            setSymbol("k");
     }
 }
