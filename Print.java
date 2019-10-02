95e2c81642856288da29035979426623411f4513
Toni <toni.heinonen@hotmail.fi>
2019-10-02 13:16:29 +0300

    Add javadoc to msg() in Print

--- Print.java
+++ Print.java
@@ -1,5 +1,9 @@
 public abstract class Print {
     
+    /** 
+     * Prints provided text surrounded by borders.
+     * @param text to print
+     */
     public static void msg(String text) {
         String border = "---";
         System.out.println(border + " " + text + " " + border);
