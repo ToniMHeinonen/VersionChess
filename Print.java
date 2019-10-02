7868e447ca0395da6d64ed476ddc0d4f04a06af2
Toni <toni.heinonen@hotmail.fi>
2019-10-02 13:15:05 +0300

    Initialize msg() in Print
    Method now prints provided text argument surrounded by borders.

--- Print.java
+++ Print.java
@@ -1,6 +1,7 @@
 public abstract class Print {
     
     public static void msg(String text) {
-
+        String border = "---";
+        System.out.println(border + " " + text + " " + border);
     }
 }
