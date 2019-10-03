04d03ec82f25dc963203544980052b87f8aa6aa7
Toni <toni.heinonen@hotmail.fi>
2019-10-03 12:50:29 +0300

    Add variable name to Player
    Name holds the player's name.

--- Player.java
+++ Player.java
@@ -1,3 +1,11 @@
 public class Player {
-    
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
