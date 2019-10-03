86bb3d8408f129da4609c2bae9cbc5696dc62dd2
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:16:29 +0300

    Add Main menu print.

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        public static void main(String[] args) {
+            int choice = 0;
+            System.out.println("*********************************");
+            System.out.println("*    Welcome to play Chess!     *");
+            System.out.println("*    1.   Play the game         *");
+            System.out.println("*    2.    Information          *");
+            System.out.println("*    3.        Quit             *");
+            System.out.println("*********************************");
+            System.out.println("Please select one of the options (1-3): ");
+
         Board chess = new Board();
         chess.play();
     }
