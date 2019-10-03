9700dd4f541ff379ce17927015c419f44dabbb5a
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:08:29 +0300

    Information for Pawn.

--- Pawn.java
+++ Pawn.java
@@ -3,6 +3,9 @@ public class Pawn extends ChessPiece {
     public Pawn(int row, int col, Player player) {
         super(row, col, player);
 
+        /**
+         * Sets symbol for Pawn.
+         */
         if (player.getNumber() == 1)
             setSymbol("P");
         else
