38ff17abe58f097f0392feee4c1ca61f49383d39
Toni <toni.heinonen@hotmail.fi>
2019-10-03 13:08:14 +0300

    Modify addChessPieces() in Board
    Use players variable to loop through both players.

--- Board.java
+++ Board.java
@@ -11,39 +11,38 @@ public class Board {
         getNames();
         
         // Add chess pieces for player 1 and 2
-        addChessPieces(1);
-        addChessPieces(2);
+        addChessPieces();
     }
 
     /**
      * Add chess pieces for players 1 and 2
-     * @param player which player owns the pieces
      */
-    private void addChessPieces(Player player) {
-        int row, pawnRow;
+    private void addChessPieces() {
+        for (Player p : players) {
+            int row, pawnRow;
 
-        if (player == 1) {
-            row = 0;
-            pawnRow = 1;
-        } else {
-            row = 7;
-            pawnRow = 6;
-        }
-        
-        positions[row][0] = new Rook(row, 0, player);
-        positions[row][1] = new Knight(row, 1, player);
-        positions[row][2] = new Bishop(row, 2, player);
-        positions[row][3] = new Queen(row, 3, player);
-        positions[row][4] = new King(row, 4, player);
-        positions[row][5] = new Bishop(row, 5, player);
-        positions[row][6] = new Knight(row, 6, player);
-        positions[row][7] = new Rook(row, 7, player);
+            if (p.getNumber() == 1) {
+                row = 0;
+                pawnRow = 1;
+            } else {
+                row = 7;
+                pawnRow = 6;
+            }
+            
+            positions[row][0] = new Rook(row, 0, p);
+            positions[row][1] = new Knight(row, 1, p);
+            positions[row][2] = new Bishop(row, 2, p);
+            positions[row][3] = new Queen(row, 3, p);
+            positions[row][4] = new King(row, 4, p);
+            positions[row][5] = new Bishop(row, 5, p);
+            positions[row][6] = new Knight(row, 6, p);
+            positions[row][7] = new Rook(row, 7, p);
 
-        // Add pawns for player 1 and 2
-        for (int i = 0; i < 8; i++) {
-            positions[pawnRow][i] = new Pawn(pawnRow, i, 1);
-            positions[pawnRow][i] = new Pawn(pawnRow, i, 2);
-        }
+            // Add pawns
+            for (int i = 0; i < 8; i++) {
+                positions[pawnRow][i] = new Pawn(pawnRow, i, p);
+            }
+        }   
     }
 
     private void getNames() {
