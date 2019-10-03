85bd773bf3525984fa30ccded47160dc824677d4
Toni <toni.heinonen@hotmail.fi>
2019-10-03 12:59:19 +0300

    Add number variable to Player
    Create getters and setters.

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 public class Player {
     private String name;
+    private int number;
 
     public Player() {
     }
@@ -11,4 +12,12 @@ public class Player {
     public void setName(String name) {
         this.name = name;
     }
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
