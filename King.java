80de6941446532b64a44522062e1af96fec80fe1
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:07:42 +0300

    Information for King.

--- King.java
+++ King.java
@@ -3,6 +3,9 @@ public class King extends ChessPiece {
     public King(int row, int col, Player player) {
         super(row, col, player);
 
+        /**
+         * Sets symbol for King.
+         */
         if (player.getNumber() == 1)
             setSymbol("K");
         else
