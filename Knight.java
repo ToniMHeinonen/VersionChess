c1e07705b310551530d985f79864382901b3e311
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:08:06 +0300

    Information for Knight.

--- Knight.java
+++ Knight.java
@@ -3,6 +3,9 @@ public class Knight extends ChessPiece {
     public Knight(int row, int col, Player player) {
         super(row, col, player);
 
+        /**
+         * Sets symbol for Knight.
+         */
         if (player.getNumber() == 1)
             setSymbol("H");
         else
