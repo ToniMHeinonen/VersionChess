a35c2f71cdbcac05ca5e3aec4075559c7294764a
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:08:50 +0300

    Information for Queen.

--- Queen.java
+++ Queen.java
@@ -3,6 +3,9 @@ public class Queen extends ChessPiece {
     public Queen(int row, int col, Player player) {
         super(row, col, player);
 
+        /**
+         * Sets symbol for Queen.
+         */
         if (player.getNumber() == 1)
             setSymbol("Q");
         else
