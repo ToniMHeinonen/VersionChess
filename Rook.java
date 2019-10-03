11dd809c2a17761594e06988a57aa9e345b7e60e
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:09:06 +0300

    Information for Rook.

--- Rook.java
+++ Rook.java
@@ -3,6 +3,9 @@ public class Rook extends ChessPiece {
     public Rook(int row, int col, Player player) {
         super(row, col, player);
 
+        /**
+         * Sets symbol for Rook.
+         */
         if (player.getNumber() == 1)
             setSymbol("R");
         else
