ccb0f221d2d40339b3c8b3bee845c4fdf90f14cc
Ossi <ollimatti.aikio@hotmail.com>
2019-10-04 01:23:02 +0300

    Add Main menu choice input between 1-3.

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        public static void main(String[] args) {
-            int choice = 0;
-            System.out.println("*********************************");
-            System.out.println("*    Welcome to play Chess!     *");
-            System.out.println("*    1.   Play the game         *");
-            System.out.println("*    2.    Information          *");
-            System.out.println("*    3.        Quit             *");
-            System.out.println("*********************************");
-            System.out.println("Please select one of the options (1-3): ");
-
-        Board chess = new Board();
-        chess.play();
+        int choice = 0;
+        System.out.println("*********************************");
+        System.out.println("*    Welcome to play Chess!     *");
+        System.out.println("*    1.   Play the game         *");
+        System.out.println("*    2.    Information          *");
+        System.out.println("*    3.        Quit             *");
+        System.out.println("*********************************");
+        System.out.println("Please select one of the options (1-3): ");
+        boolean choices = true;
+        while (choices) {
+            choice = Helpers.inputInt();
+            if (choice > 3 || choice < 1) {
+                System.out.println("Invalid input! Choose one of the three options.");
+            } else {
+                choices = false;
+            }
+        }
     }
 }
