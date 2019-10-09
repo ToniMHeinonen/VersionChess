6a79da070a9b0fdc10321a67bd3ebeb2721c097d
Toni <toni.heinonen@hotmail.fi>
2019-10-09 13:46:42 +0300

    Modify canMove() in Pawn.
    Check for movement in row only is ready for player 1.

--- Pawn.java
+++ Pawn.java
@@ -24,20 +24,45 @@ public class Pawn extends ChessPiece {
         if (colAmount > 1 || colAmount < -1)
             return false;
 
-        // Pawn has to move in rows
-        if (rowAmount == 0)
+        // Pawn has to move in rows and can never move more than 2
+        if (rowAmount == 0 || rowAmount > 2 || rowAmount < -2)
             return false;
         
         if (getPlayer().getNumber() == 1) {
             // Player 1 can't move down
             if (rowAmount < 0)
                 return false;
+
+            // If player only moved forwards, check only rows
+            if (colAmount == 0) {
+                // Pawn can't eat pieces in front
+                if (positions[row][col] != null) {
+                    return false;
+                }
+                // If only one row was moved up
+                if (rowAmount == 1) {
+                    return true;
+                }
+                // If two rows were moved
+                else if (rowAmount == 2) {
+                    // Pawn can only move 2 in row 1
+                    if (getRow() != 1)
+                        return false;
+                    
+                    /* If next position is null, return true, since
+                       target position was already checked */
+                    if (positions[row+1][col] == null)
+                        return true;
+                }
+            }
             
         } else {
-            // Player 1 can't move up
+            // Player 2 can't move up
             if (rowAmount > 0)
                 return false;
+
+            
         }
-        return true;
+        return false;
     }
 }
