7148f3e2fa5c63b1e56eff80fabe922833cc3625
Ossi <ollimatti.aikio@cs.tamk.fi>
2019-10-10 10:15:17 +0300

    Add javadoc to eat() in Knight class.

--- Knight.java
+++ Knight.java
@@ -25,6 +25,10 @@ public class Knight extends ChessPiece {
             return true;
     }
 
+    /**
+     * Determines if King can eat opponents piece.
+     * @return returns true if King can eat.
+     */
     public boolean eat() {
         return true;
     }
