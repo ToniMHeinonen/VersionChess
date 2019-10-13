5081c9f74e643787766c42e3de688cb0d783a49d
Toni <toni.heinonen@hotmail.fi>
2019-10-13 12:31:05 +0300

    Add new state MOVING to Board.

--- Board.java
+++ Board.java
@@ -5,7 +5,7 @@ public class Board {
     private Player playerTurn;
     private ChessPiece selectedPiece;
     private int state;
-    private final int MOVE_FROM = 0, MOVE_TO = 1;
+    private final int MOVE_FROM = 0, MOVE_TO = 1, MOVING = 2;
     /*
     moveFromColRow[] contains two values: column and row.
     These values are the chesspiece's position, which
