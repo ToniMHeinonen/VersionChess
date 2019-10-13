70d3873b51fc329f53a816d20842af4aa12023c1
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-13 19:27:17 +0300

    Add new condition to Bishop's movement.
    If Bishop is moving down and left, check all the spots between start and end point.

--- Bishop.java
+++ Bishop.java
@@ -30,6 +30,14 @@ public class Bishop extends ChessPiece {
                 }
             }
         }
+
+        if (colAmount < -1 && rowAmount < -1) {
+            for (int i = -2; i > colAmount; i--) {
+                if (positions[getRow()+i+1][getCol()+i+1] != null) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
