48b89c8584313fe12286bc0d8c9d1bd96cd64c33
Toni <toni.heinonen@hotmail.fi>
2019-10-19 10:32:15 +0300

    Initialize copyPositions() in Helper.

--- Helper.java
+++ Helper.java
@@ -1,5 +1,7 @@
 public abstract class Helper {
     public static ChessPiece[][] copyPositions(ChessPiece[][] positions) {
-        
+        ChessPiece[][] copy = new ChessPiece[positions.length][positions.length];
+        System.arraycopy(positions, 0, copy, 0, copy.length);
+        return copy;
     }
 }
