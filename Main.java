967be390a7592a39b7099834f702d21a4796e2b5
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-21 19:05:01 +0300

    Change max num to 4 in Main class.

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
             Print.startScreen();
-            int choice = Input.getInt(1, 3, "Invalid input! Choose one of the three options.");
+            int choice = Input.getInt(1, 4, "Invalid input! Choose one of the three options.");
 
             switch (choice) {
 
