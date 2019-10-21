e447732515decffcc85b6d9c7bc1b535622d6ee7
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-21 19:04:32 +0300

    Add break to case 4.

--- Main.java
+++ Main.java
@@ -24,6 +24,7 @@ public class Main {
 
                 case 4: {
                     TicTacToe tictactoe = new TicTacToe();
+                    break;
                 }
             }
         }
