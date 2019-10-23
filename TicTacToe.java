549b118e35f37fdc0b83fd50125ac6de8fdf050b
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-23 18:42:04 +0300

    Initialize checkWin() in TicTacToe.
    Check if player has 3 marks in a row.

--- TicTacToe.java
+++ TicTacToe.java
@@ -27,6 +27,16 @@ public class TicTacToe {
     }
 
     public void checkWin() {
-        
+        int counter = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (TTTBoard[i][j] == "X") {
+                    counter++;
+                } else {
+                    counter = 0;
+                }
+            }
+        }
     }
 }
