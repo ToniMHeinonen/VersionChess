ccc11842856b5d52318af4460d60ea8f1b3b7bed
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-26 07:36:51 +0300

    Initialize aiTurn() in TicTacToe class.
    If there is an empty spot, put AI's mark to it.

--- TicTacToe.java
+++ TicTacToe.java
@@ -30,7 +30,14 @@ public class TicTacToe {
     }
 
     public void aiTurn() {
-        
+        for (int i = 0; i < 3; i ++) {
+            for (int j = 0; j < 3; j++) {
+                if (TTTBoard[i][j] == null) {
+                    TTTBoard[i][j] = AIMark;
+                    break;
+                }
+            }
+        }
     }
 
     public boolean checkWin(String mark) {
