2c815e407dd331812a8d18d77748cf6a40dfc0c9
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-27 16:35:45 +0200

    Modify checkDraw() in TicTacToe class.
    Change boolean values.

--- TicTacToe.java
+++ TicTacToe.java
@@ -46,12 +46,12 @@ public class TicTacToe {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (TTTBoard[i][j] == null) {
-                    return true;
+                    return false;
                 }
             }
         }
 
-        return false;
+        return true;
     }
 
     public void aiTurn() {
