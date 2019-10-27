2d717dfb78e5647a04cc84ffdc330aaef356a3de
OAhonen <olli.p.ahonen@tuni.fi>
2019-10-27 16:33:40 +0200

    Initialize checkDraw() in TicTacToe class.
    Check if there are spots free in the board.

--- TicTacToe.java
+++ TicTacToe.java
@@ -39,6 +39,15 @@ public class TicTacToe {
     }
 
     public boolean checkDraw() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (TTTBoard[i][j] == null) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public void aiTurn() {
